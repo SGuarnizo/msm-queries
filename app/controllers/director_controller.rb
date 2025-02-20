7d2fec7cf28a062393d41f854f13ac365017cb4c
SGuarnizo <sguarniz@chicagobooth.edu>
2025-02-20 00:18:48 +0000

    details directors

--- app/controllers/director_controller.rb
+++ app/controllers/director_controller.rb
@@ -3,4 +3,12 @@ class DirectorController < ApplicationController
     @num_directors = Director.all 
     render({ :template => "director_templates/director" })
   end
+
+  def details
+    @director_id = params.fetch("an_id").to_i
+    x_i = Director.where({ :id => @director_id})
+    @x = x_i.at(0)
+    render({ :template => "director_templates/details" })
+  end
+
 end
