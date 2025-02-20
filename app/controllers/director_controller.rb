570ba61d86ccb692efa941c5413ac981372c87f8
SGuarnizo <sguarniz@chicagobooth.edu>
2025-02-20 00:27:12 +0000

    director complete

--- app/controllers/director_controller.rb
+++ app/controllers/director_controller.rb
@@ -8,6 +8,7 @@ class DirectorController < ApplicationController
     @director_id = params.fetch("an_id").to_i
     x_i = Director.where({ :id => @director_id})
     @x = x_i.at(0)
+    @movie_array= Movie.where({ :director_id => @director_id})
     render({ :template => "director_templates/details" })
   end
 
